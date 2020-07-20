36af95717555b2f0474c658c8387152a42ebf941
NouKD <NouKD-n09916538@gmail.com>
2020-07-20 03:01:54 +0000

    update

--- fasty/settings.py
+++ fasty/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 django_heroku.settings(locals())
