7c4575479548e001c24b2280391a9c2c32e2ae65
NouKD <NouKD-n09916538@gmail.com>
2020-07-20 00:52:14 +0000

    heroku deploiment

--- concepte/fasty/fasty/settings.py
+++ concepte/fasty/fasty/settings.py
@@ -51,7 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #config whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'fasty.urls'
 
